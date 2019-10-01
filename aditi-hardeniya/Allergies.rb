18ec1179eac1ad7522a426fae4d7e0a826634e1a
Aditi.Hardeniya <Aditi.Hardeniya@diasparkindia.com>
2019-10-01 18:57:51 +0530

    Made the requested changes in assignment 1

--- aditi-hardeniya/Allergies.rb
+++ aditi-hardeniya/Allergies.rb
@@ -1,69 +1,47 @@
-
-  $list = Array.new;
+puts"Enter allergic store:"
+sc=gets.to_i
+puts"Enter Item:"
+item=gets.chomp
+$list = Array.new;
+$num_list=[128,64,32,16,8,4,2,1]
+$item_list=['cats','pollen','chocolate','tomatoes','strawberries','shellfish','peanuts','eggs']
     def foo(score)
-           # $list = Array.new;
-        $score=score
-            #ignore non-$listergen numbers
-            $score = $score % 256
-            
-            if $score >= 128
-                $list.push('cats')
-                $score -= 128
-            end
-            if $score >= 64
-                $list.push('pollen') 
-                $score -= 64
-            end
-            if $score >= 32
-             $list.push('chocolate')
-                $score -= 32
-            end
-            if $score >= 16
-                $list.push('tomatoes')
-                $score -= 16
-            end
-            if $score >= 8
-                $list.push('strawberries')
-                $score -= 8
-            end
-            if $score >= 4
-                $list.push('shellfish')
-                $score -= 4
-            end
-            if $score >= 2
-                $list.push('peanuts')
-                $score -= 2
-            end
-            if $score >= 1
-                $list.push('eggs')
-            end
-            $list.reverse()        
-        end
-        def is_allergic_to(item)
-            $allergic=false
+         #ignore non-listergen numbers
+     score = score % 256
+     for i in 0 .. 7
+         if(score >=$num_list[i])
+             score=score-($num_list[i])
+             $list.push($item_list[i])
+               
+         end
+     end
 
-            if (
-                $list.each { |ty|
-                    puts ty
-                    if(ty.to_s == item.to_s)
-                        $allergic =   true 
-                        break;
-                    else
-                        $allergic = false
+
+     $list.reverse()        
+    end
+    def is_allergic_to(items)
+     allergic=false
+     if ($list.each { |t|
+            # puts t
+        if(t.to_s == items.to_s)
+             allergic =   true 
+             break;
+         else
+             allergic = false
                         
-                    end
-                }
-            )
-
-            end
-   return  $allergic
         end
+         })
+        
+
+     end
+    return  allergic
+    end
     
         
-    foo(130); 
-pp = is_allergic_to("cats")
-   puts pp
+foo(sc); 
+yes_or_no = is_allergic_to(item)
+puts yes_or_no
 
-puts "The given list of allergies:"
- $list.each {|ty|
-    puts ty}
+puts "The given list of allergies"
+$list.each {|ty|
+puts ty}
