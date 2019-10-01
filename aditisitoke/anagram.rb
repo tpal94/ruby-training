a212fc80a491a4edd52911a1f7f992fd03c95c12
aditi-sitoke <aditi.sitoke@diaspark.com>
2019-10-01 16:37:59 +0530

    updated assignment 1

--- aditisitoke/anagram.rb
+++ aditisitoke/anagram.rb
@@ -1,89 +1,29 @@
-
-my_string = "listen"
+puts "Enter a string !!"
+my_string = gets.chomp
 my_string.split
 
-#my_string = "listen";
-counter = 0;
-counter1 = 0;
-counter2 = 0;
-counter3 = 0;
-
-w = "enlists";
-x = "google";
-y = "inlets";
-z = "banana";
+x = Array.new(4)
+for a in 0..3
+puts "Enter the word you want to check for anagram !!"
+x = gets.chomp
 
 i=0;
-if(w.length == my_string.length)
+counter = 0;
+
+if(x.length == my_string.length)
 while (i<my_string.length) 
-   if (w.include?my_string[i])
+   if (x.include?my_string[i])
     counter = counter+1;
    end
     i= i+1;
-    #puts counter;
 end
 end
 if (counter == my_string.length)
-    puts "#{w} is an anagram of #{my_string}";
-else
-    puts "#{w} is not an anagram of #{my_string}";
-end
-
-
-
-
-
-i=0;
-if(x.length == my_string.length)
-while (i<my_string.length) do
-    if (x.include?my_string[i])
-     counter1 = counter1+1;
-    end
-     i= i+1;
-end
- #puts counter1
-end
-if (counter1 == my_string.length)
     puts "#{x} is an anagram of #{my_string}";
 else
     puts "#{x} is not an anagram of #{my_string}";
-
 end
 
+end #for end
 
 
-
-i=0;
-if(y.length == my_string.length)
-while (i<my_string.length) do
-    if (y.include?my_string[i])
-     counter2 = counter2+1;
-    end
-     i= i+1;
-end
- #puts counter2
-end
-if (counter2 == my_string.length)
-    puts "#{y} is an anagram of #{my_string}";
-else
-    puts "#{y} is not an anagram of #{my_string}";
-end
-
-
-
-
-i=0;
-if(z.length == my_string.length)
-while (i<my_string.length) 
-    if (z.include?my_string[i])
-     counter3 = counter3+1;
-    end
-     i= i+1;
-end
- #puts counter3
-end 
-if (counter3 == my_string.length)
-    puts "#{z} is an anagram of #{my_string}";
-else
-    puts "#{z} is not an anagram of #{my_string}";
-end
