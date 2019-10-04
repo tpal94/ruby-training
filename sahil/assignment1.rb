dabd1084f7f06370cf3a84dff12d6e6dc7b2ad14
sahilgupta0902 <51989399+sahilgupta0902@users.noreply.github.com>
2019-10-04 13:55:19 +0530

    Create assignment1.rb

---
+++ sahil/assignment1.rb
@@ -0,0 +1,26 @@
+class Gadi 
+    
+    Total_min = 24*60
+    @minutes=0
+    def set_times(hours, minutes = 0)
+        @minutes=60 * hours + minutes     
+        @minutes
+   
+    end
+  
+    def add_minutes(minutes)
+      @minutes = (@minutes + minutes) % Total_min
+    end
+  
+    def subtract_minutes(minutes)
+        @minutes = (@minutes - minutes) % Total_min
+     
+    end
+
+    
+    def to_print
+      "#{@minutes / 60}:#{@minutes % 60}"
+    end
+  
+   
+  end
