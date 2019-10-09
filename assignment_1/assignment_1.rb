90ac64d44f4066b0d7b60470bdcc0ee6c54633af
sahilgupta0902 <51989399+sahilgupta0902@users.noreply.github.com>
2019-10-09 12:23:30 +0530

    Create assignment_1.rb

---
+++ assignment_1/assignment_1.rb
@@ -0,0 +1,57 @@
+puts "Enter hours and minutes "
+hours = gets
+minutes = gets
+$day =  hours.to_i * 60 +   minutes.to_i;
+puts "current time is : #{  $day.to_i / 60 % 24}:#{  $day.to_i % 60}" 
+puts "1.ADD MINUTES  \n2.SUBTRACT MINUTES \n3.check equal time "
+def add(min)
+    $day =   $day.to_i + min.to_i
+    puts "#{  $day.to_i / 60 % 24}:#{  $day.to_i % 60}" 
+end
+def minus(min)
+    $day =   $day.to_i - min.to_i
+    puts "#{  $day.to_i / 60 % 24}:#{  $day.to_i % 60}"
+end
+def check_equal(mn)
+    if(  $day.to_i == mn.to_i )
+        puts"clocks are equal"
+    else
+        puts"clocks are not equal"
+    end
+end
+$pr=1
+while($pr==1) 
+    $pr=0
+    str = gets.to_i 
+# using case statement  
+   case str  
+  
+# using when 
+        when 1 
+        puts"enter minutes : "
+        a=gets.to_i
+        add(a)
+        
+        when 2
+        puts"enter minutes : "
+        b=gets.to_i
+        minus(b)
+  
+        when 3  
+        puts"enter clock time to compare : "
+        hrr=gets.to_i
+        mnn=gets.to_i
+        mnn=hrr.to_i * 60 + mnn.to_i;
+        check_equal(mnn) 
+
+
+        when 4  
+        break;
+        else  
+        puts "wrong option"
+  
+    end
+puts "press 1 to continue"
+$pr=gets.to_i
+
+end
