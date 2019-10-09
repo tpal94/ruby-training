ce146499e612d4c633b7b3046b221632591c6907
aditi-sitoke <aditi.sitoke@diaspark.com>
2019-10-09 16:48:50 +0530

    updated assignment 2

--- atbash.rb
+++ atbash.rb
@@ -1,42 +1,22 @@
-module Atbash
+class Atbash
 
-
-# puts "Enter text to decode or decode"
-# @@str = gets.chomp.downcase.gsub(/\s+/, "");
-
-def self.decode(str)
-plain = Array("a".."z");
-cipher = Array.new;
-    i = 0;
-    j = 0;    
-    while(i<str.length)
-        j=0;
-        while(j<plain.length)
-            if(str[i]==plain[j])
-                 cipher.insert(i, plain[25-j]);
-                break;
-            else
-            j=j+1;
-            end
+    puts "Enter text to decode or decode"
+    @@str = gets.chomp.downcase.gsub(/\s+/, "");
+    
+    def decode
+    plain = @@str.reverse
+    cipher = Array.new;
+        i = 0;
+        while(i<@@str.length)
+           @@str[i]=plain[i]
+                     cipher.insert(i, @@str[i]);
+                i=i+1;
         end
-            i=i+1;
-    end
-    ch = cipher[0];
-    i=1
-    while(i<cipher.length)
-        ch = ch + cipher[i];
-        i=i+1;
-    end
-return ch;
-
-# rescue NoMethodError => e
-#     print_exception(e, true)
-# rescue => e
-#     print_exception(e,false)
-
-
-end #function
-end #class
-
-# obj = Atbash.new
-# obj.decode
+    
+    puts cipher;
+    
+    end #function
+    end #class
+    
+    obj = Atbash.new
+    obj.decode
