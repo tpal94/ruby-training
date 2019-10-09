951ca8d9a03e5f81a379b132155b2ea297b9843f
Dewanshu-Chhatlani <55981827+Dewanshu-Chhatlani@users.noreply.github.com>
2019-10-09 15:33:13 +0530

    Made the suggested optimization.

--- Dewanshu-Assignment1/poem.rb
+++ Dewanshu-Assignment1/poem.rb
@@ -8,26 +8,11 @@ class Poem
             return "Enter positive number of bottles.";
         else
             while(@bottles>=0)
-                if(@bottles>2)
-                    puts "#{@bottles} bottles of beer on the wall, #{@bottles} bottles of beer.";
-                    puts "Take one down and pass it around, #{@bottles-1} bottles of beer on the wall.";
-                    puts " ";
-                    count = count + 2;
-                elsif(@bottles==2)
-                    puts "#{@bottles} bottles of beer on the wall, #{@bottles} bottles of beer.";
-                    puts "Take one down and pass it around, #{@bottles-1} bottle of beer on the wall.";
-                    puts " ";
-                    count = count + 2;
-                elsif(@bottles==1)
-                    puts "#{@bottles} bottle of beer on the wall, #{@bottles} bottle of beer.";
-                    puts "Take one down and pass it around, no more bottles of beer on the wall.";
-                    puts " ";
-                    count = count + 2;
-                else
-                    puts "No more bottles of beer on the wall, no more bottles of beer."
-                    puts "Go to the store and buy some more, #{i} bottles of beer on the wall."
-                    count = count + 2;
-                end
+                puts "#{@bottles>0?@bottles : "No more"} bottles of beer on the wall, #{@bottles>0 ? @bottles : "No more"} bottles of beer"
+                print "#{@bottles>0?"Take one down and pass it around, ":"Go to the store and buy some more, "}"
+                puts "#{@bottles==1? "No more" : @bottles>1?@bottles-1:i} bottles of beer on the wall"
+                puts " "
+                count = count + 2;
                 @bottles=@bottles-1;
             end
             return count;
