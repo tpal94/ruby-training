1fc37d23f9e40594175e9e0aa7facc9599ee5667
deepak-diaspark <51991554+deepak-diaspark@users.noreply.github.com>
2019-10-10 20:38:10 +0530

    Assignment 3 added

---
+++ deepak-parmar/GNewsFeed.rb
@@ -0,0 +1,84 @@
+require 'news-api'
+require 'net/http'
+require 'json'
+require 'launchy'
+url = 'http://newsapi.org/v2/top-headlines?sources=google-news&apiKey=977325ca24414ca0aa47a5a8c6e24e32'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+json = JSON.parse(response)
+
+header = '<!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title>News API</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+        * {
+        box-sizing: border-box;
+        }
+        body {
+        font-family: Arial, Helvetica, sans-serif;
+        }
+        /* Style the header */
+        header {
+        height: 150px;
+        background-color: #000;
+        padding: 15px;
+        text-align: center;
+        font-size: 35px;
+        color: white;
+        }
+        article {
+        float: left;
+        padding: 20px;
+        width: 100%;
+        }
+        /* Clear floats after the columns */
+        section:after {
+        content: "";
+        display: table;
+        clear: both;
+        }
+        /* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
+        @media (max-width: 600px) {
+        nav, article {
+            width: 100%;
+            height: auto;
+        }
+        }
+        </style>
+        </head>
+        <body>
+        <header>
+        <h5>News API</h5>
+        </header>
+        <section>
+        <article>'
+        
+body = '<b>Title:</b>&nbsp;<font size="-0.7">'+json['articles'][0]['title']+'</font><br>
+        <b>Author:</b>&nbsp;<font size="-0.7"></font><br>
+        <b>Published At:</b>&nbsp;<font size="-0.7">'+json['articles'][0]['publishedAt']+'</font><br>
+        <b>Description:</b>&nbsp;<font size="-0.7">'+json['articles'][0]['description']+'</font><br>
+        <b>Url:</b>&nbsp;<font size="-0.7"><a href="'+json['articles'][0]['url']+'">Go To Url</a></font><br>
+        <b>Image:</b>&nbsp;<font size="-0.7"><a href="'+json['articles'][0]['urlToImage']+'">Image</a></font><br>
+        <b>Content:</b>&nbsp;<font size="-0.7">'+json['articles'][0]['content']+'</font><br><br><br>
+        
+        <b>Title:</b>&nbsp;<font size="-0.7">'+json['articles'][1]['title']+'</font><br>
+        <b>Author:</b>&nbsp;<font size="-0.7"></font><br>
+        <b>Published At:</b>&nbsp;<font size="-0.7">'+json['articles'][1]['publishedAt']+'</font><br>
+        <b>Description:</b>&nbsp;<font size="-0.7">'+json['articles'][1]['description']+'</font><br>
+        <b>Url:</b>&nbsp;<font size="-0.7"><a href="'+json['articles'][1]['url']+'">Go To Url</a></font><br>
+        <b>Image:</b>&nbsp;<font size="-0.7"><a href="'+json['articles'][1]['urlToImage']+'">Image</a></font><br>
+        <b>Content:</b>&nbsp;<font size="-0.7">'+json['articles'][1]['content']+'</font><br>'
+        
+
+footer = '</article>
+            </section>
+            </body>
+            </html>'
+
+data = header + body + footer;
+aFile = File.open("news.html", "w")
+aFile.syswrite(data)
+Launchy::Browser.run("../news.html")
